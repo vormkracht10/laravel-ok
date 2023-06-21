f796c82a22cd8ed0a3f9b16f08c8614b418f5cae
Bas van Dinther <bas@vormkracht10.nl>
2023-06-21 09:35:50 +0200

    Check for element on page (wip)

---
+++ src/Checks/Traits/ChecksForElementOnPage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Vormkracht10\LaravelOK\Checks\Traits;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Vormkracht10\LaravelOK\Checks\Base\Result;
+
+trait ChecksForElementOnPage
+{
+    public function checkExpectedElement(string $element)
+    {
+        // TODO: Pass the response body to the crawler
+        $crawler = new Crawler();
+
+        $elementIsPresent = $crawler->filter($element)->count() > 0;
+
+        if (! $elementIsPresent) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function run(): Result
+    {
+        $element = $this->queryElement(); // This is the method that is not implemented yet
+
+        $result = Result::new();
+
+        return $this->checkExpectedElement($element)
+            ? $result->ok()
+            : $result->failed(
+                $this->getMessage() ?: "The element could not be found on the page"
+            );
+    }
+}
