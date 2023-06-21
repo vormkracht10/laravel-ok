ee1089aa8ca1077a748465a20bbbf9b50198cd8d
Bas van Dinther <bas@vormkracht10.nl>
2023-06-21 15:06:59 +0200

    Complete setup for the elements on page check

--- src/Checks/Traits/ChecksForElementOnPage.php
+++ src/Checks/Traits/ChecksForElementOnPage.php
@@ -2,15 +2,38 @@
 
 namespace Vormkracht10\LaravelOK\Checks\Traits;
 
+use Exception;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\DomCrawler\Crawler;
+use Spatie\Health\Exceptions\InvalidCheck;
 use Vormkracht10\LaravelOK\Checks\Base\Result;
 
 trait ChecksForElementOnPage
 {
-    public function checkExpectedElement(string $element)
+    protected ?string $url = null;
+
+    /** @var array<string, string> */
+    protected array $headers = [];
+
+    public function url(string $url): self
     {
-        // TODO: Pass the response body to the crawler
-        $crawler = new Crawler();
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /** @param  array<string, string>  $headers */
+    public function headers(array $headers = []): self
+    {
+        $this->headers = $headers;
+
+        return $this;
+    }
+
+    public function checkExpectedElement(Response $response, string $element)
+    {
+        $crawler = new Crawler($response->body());
 
         $elementIsPresent = $crawler->filter($element)->count() > 0;
 
@@ -21,16 +44,31 @@ trait ChecksForElementOnPage
         return true;
     }
 
+    /** @throws InvalidCheck */
+    public function getUrlResponse(): Response
+    {
+        try {
+            $response = Http::withHeaders($this->headers)
+                ->send('GET', $this->url);
+
+            if (! $response->ok()) {
+                throw InvalidCheck::couldNotSendRequest($response);
+            }
+        } catch (Exception $exception) {
+            throw InvalidCheck::couldNotSendRequest($exception);
+        }
+
+        return $response;
+    }
+
     public function run(): Result
     {
-        $element = $this->queryElement(); // This is the method that is not implemented yet
-
         $result = Result::new();
 
-        return $this->checkExpectedElement($element)
+        return $this->checkExpectedElement($this->getUrlResponse(), $this->element)
             ? $result->ok()
             : $result->failed(
-                $this->getMessage() ?: "The element could not be found on the page"
+                $this->getMessage() ?: "The element '{$this->element}' could not be found on the page '{$this->url}'"
             );
     }
 }
