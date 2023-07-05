2ada66274ded3d141129d91344ef5302cc3ca59c
Bas van Dinther <bas@vormkracht10.nl>
2023-07-05 11:40:32 +0200

    wip

--- src/Checks/Traits/ChecksForElementOnPage.php
+++ src/Checks/Traits/ChecksForElementOnPage.php
@@ -72,7 +72,7 @@ class ChecksForElementOnPage extends Check
         $element = $crawler->filter($element);
 
         if ($this->attribute) {
-            $element = $crawler->filterXPath("//{$element}")->extract([$this->attribute]);
+            $element = $crawler->filterXPath("//{$element}")->getNode(0);
         }
 
         if (! $element) {
