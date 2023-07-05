69a382e0a2ae19a7f68bb71d7f60ad53f0367127
Bas van Dinther <bas@vormkracht10.nl>
2023-07-05 11:43:14 +0200

    wip

--- src/Checks/Traits/ChecksForElementOnPage.php
+++ src/Checks/Traits/ChecksForElementOnPage.php
@@ -93,11 +93,13 @@ class ChecksForElementOnPage extends Check
 
         // Check if attribute has text
         if (! is_null($this->attribute) && ! is_null($this->text)) {
+            dd($element);
             foreach ($element as $e) {
-                dd($e, $e->attr($this->attribute), $this->text);
-                if ($e->attr($this->attribute) == $this->text) {
-                    return true;
-                }
+                dd($e);
+                // dd($e, $e->attr($this->attribute), $this->text);
+                // if ($e->attr($this->attribute) == $this->text) {
+                //     return true;
+                // }
             }
         }
 
