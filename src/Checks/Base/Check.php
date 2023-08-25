f258ca30e80503a23bee28264772fe322a56785d
dulkoss <75451291+dulkoss@users.noreply.github.com>
2023-08-25 13:52:05 +0200

    Add view and view data fields to Check.

--- src/Checks/Base/Check.php
+++ src/Checks/Base/Check.php
@@ -12,6 +12,20 @@ abstract class Check
 {
     use ManagesFrequencies;
 
+    /**
+     * The view name used for mail notifications.
+     *
+     * @var string
+     */
+    public string $view = 'notifications::email';
+
+    /**
+     * The data passed to the view for mail notifications.
+     *
+     * @var array
+     */
+    public array $data = [];
+
     protected string $expression = '* * * * *';
 
     protected ?string $name = null;
