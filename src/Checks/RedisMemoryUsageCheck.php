620b1c311bd8757d409769ec8335dfb6eb93d772
David <75451291+dulkoss@users.noreply.github.com>
2023-09-15 16:10:23 +0200

    add RedisMemoryUsageCheck

---
+++ src/Checks/RedisMemoryUsageCheck.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Vormkracht10\LaravelOK\Checks;
+
+use Illuminate\Redis\Connections\PhpRedisConnection;
+use Illuminate\Redis\Connections\PredisConnection;
+use Illuminate\Support\Facades\Redis;
+use Vormkracht10\LaravelOK\Checks\Base\Check;
+use Vormkracht10\LaravelOK\Checks\Base\Result;
+
+class RedisMemoryUsageCheck extends Check
+{
+    protected int $threshold;
+
+    public function gigabytes($amount): static
+    {
+        return $this->threshold($amount * (10 ** 9));
+    }
+
+    public function megabytes($amount): static
+    {
+        return $this->threshold($amount * (10 ** 6));
+    }
+
+    public function threshold(int $bytes): static
+    {
+        $this->threshold = $bytes;
+
+        return $this;
+    }
+
+    public function run(): Result
+    {
+        $result = Result::new();
+
+        $bytes = $this->memoryUsageInBytes();
+
+        $readable = round($bytes / (10 ** 6), 2);
+        $readableMax = $this->threshold / (10 ** 6);
+
+        echo $this->threshold;
+
+        if ($bytes >= $this->threshold) {
+            return $result->failed("Memory usage is {$readable}MB, max is configured at {$readableMax}MB");
+        }
+
+        return $result->ok("Memory usage is at {$readable}MB");
+    }
+
+    public function memoryUsageInBytes(): int
+    {
+        $redis = Redis::connection();
+
+        return match ($redis::class) {
+            PhpRedisConnection::class => $redis->info()['used_memory'],
+            PredisConnection::class => $redis->info()['Memory']['used_memory'],
+            default => null,
+        };
+    }
+}
