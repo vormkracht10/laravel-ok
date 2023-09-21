7cd56a4dab051d7dfa8f683734099013db793813
David <75451291+dulkoss@users.noreply.github.com>
2023-09-21 13:27:43 +0200

    silence QueueHeartbeatJob

--- src/LaravelOKServiceProvider.php
+++ src/LaravelOKServiceProvider.php
@@ -8,6 +8,7 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Vormkracht10\LaravelOK\Commands\DispatchQueueCheckJobsCommand;
 use Vormkracht10\LaravelOK\Commands\RunChecksCommand;
 use Vormkracht10\LaravelOK\Events\CheckFailed;
+use Vormkracht10\LaravelOK\Jobs\QueueHeartbeatJob;
 use Vormkracht10\LaravelOK\Listeners\SendCheckFailedNotification;
 
 class LaravelOKServiceProvider extends PackageServiceProvider
@@ -32,11 +33,31 @@ class LaravelOKServiceProvider extends PackageServiceProvider
             });
     }
 
+    public function packageBooted()
+    {
+        $this->silenceQueueHeartbeatJob();
+    }
+
     public function packageRegistered(): void
     {
         $this->app->singleton(OK::class);
         $this->app->alias(OK::class, 'ok');
 
         $this->app['events']->listen(CheckFailed::class, SendCheckFailedNotification::class);
+    }
+
+    protected function silenceQueueHeartbeatJob(): static
+    {
+        $silencedJobs = config('horizon.silenced', []);
+
+        if (in_array(QueueHeartbeatJob::class, $silencedJobs)) {
+            return $this;
+        }
+
+        $silencedJobs[] = QueueHeartbeatJob::class;
+
+        config()->set('horizon.silenced', $silencedJobs);
+
+        return $this;
     }
 }
