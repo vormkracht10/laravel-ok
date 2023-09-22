7d6e592bfd2c9b134aee4eac626404432d1c47cb
David <75451291+dulkoss@users.noreply.github.com>
2023-09-22 11:41:16 +0200

    add DatabaseConnectionCountCheck

---
+++ src/Checks/DatabaseConnectionCountCheck.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Vormkracht10\LaravelOK\Checks;
+
+use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
+use Vormkracht10\LaravelOK\Checks\Base\Check;
+use Vormkracht10\LaravelOK\Checks\Base\Result;
+
+class DatabaseConnectionCountCheck extends Check
+{
+    protected ?string $connection = null;
+
+    protected int $maxConnections = 50;
+
+    public function withConnection(string $name): static
+    {
+        $this->connection = $name;
+
+        return $this;
+    }
+
+    public function setMaxConnections($value): static
+    {
+        $this->maxConnections = $value;
+
+        return $this;
+    }
+
+    public function run(): Result
+    {
+        $result = Result::new();
+
+        $connection = $this->connection ?? config('database.default');
+
+        if (($count = $this->getConnectionCount($connection)) > $this->maxConnections) {
+            return $result->failed("Too many database connections ({$count})");
+        }
+
+        return $result->ok("{$count} connections connected to the database");
+    }
+
+    protected function getConnectionCount(string $connectionName): int
+    {
+        $connection = app(ConnectionResolverInterface::class)->connection($connectionName);
+
+        return match (true) {
+            $connection instanceof MySqlConnection => (int) $connection->selectOne('show status where variable_name = "threads_connected"')->Value,
+            $connection instanceof PostgresConnection => (int) $connection->selectOne('select count(*) as connections from pg_stat_activity')->connections,
+            default => throw new \RuntimeException("Connection [{$connectionName}] not supported")
+        };
+    }
+}
