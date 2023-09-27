08c4990bf2267a96cd3c3e9e91d17ca477a290f2
David <75451291+dulkoss@users.noreply.github.com>
2023-09-27 15:53:59 +0200

    remove 'exact' option.

--- src/Checks/ProcessCountCheck.php
+++ src/Checks/ProcessCountCheck.php
@@ -12,7 +12,7 @@ class ProcessCountCheck extends Check
 
     public function setProcessCountThresholds(array $config): static
     {
-        $this->processCountThresholds = $this->processConfig($config);
+        $this->processCountThresholds = $config;
 
         return $this;
     }
@@ -23,51 +23,24 @@ class ProcessCountCheck extends Check
 
         $failed = [];
 
-        foreach ($this->processCountThresholds as $process => $config) {
-            $count = $this->getProcessCountForName($process, $config['exact'] ?? true);
+        foreach ($this->processCountThresholds as $process => $max) {
+            $count = $this->getProcessCountForName($process);
 
-            if ($count > $config['max']) {
+            if ($count > $max) {
                 $failed[] = "{$process} ({$count})";
             }
         }
 
-        if (! empty($failed)) {
-            return $result->failed('Some commands have more processes running than should be allowed: ['.implode(', ', $failed).']');
+        if (!empty($failed)) {
+            return $result->failed('Some commands have more processes running than should be allowed: [' . implode(', ', $failed) . ']');
         }
 
         return $result->ok('All processes are ok');
     }
 
-    protected function processConfig(array $config): array
+    protected function getProcessCountForName(string $name): int
     {
-        $reconstructed = [];
-
-        foreach ($config as $process => $processConfig) {
-            if (! is_array($processConfig) && ! is_int($processConfig)) {
-                throw new \Exception('The config for each process has to be either an integer (max process count) or an array');
-            }
-
-            if (is_int($processConfig)) {
-                $processConfig = ['max' => $processConfig];
-            }
-
-            if (! isset($processConfig['max']) || ! is_int($processConfig['max'])) {
-                throw new \Exception('The configured max process count has to be an integer');
-            }
-
-            $reconstructed[$process] = $processConfig;
-        }
-
-        return $reconstructed;
-    }
-
-    protected function getProcessCountForName(string $name, bool $exact = true): int
-    {
-        $command = $exact
-            ? "ps -e | awk '{print $4}' | sed 's:.*/::' | grep '^{$name}$'"
-            : "ps -e | awk '{print $4}' | sed 's:.*/::' | grep '{$name}'";
-
-        $process = Process::run($command);
+        $process = Process::run("ps -e | awk '{print $4}' | sed 's:.*/::' | grep '^{$name}$'");
 
         if (! $process->successful()) {
             return 0;
