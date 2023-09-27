0ecb36d6e59d3eec871a445aa569152bfc8684c5
David <75451291+dulkoss@users.noreply.github.com>
2023-09-27 15:16:17 +0200

    add DatabaseTableSizeCheck

---
+++ src/Checks/DatabaseTableSizeCheck.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Vormkracht10\LaravelOK\Checks;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
+use Vormkracht10\LaravelOK\Checks\Base\Check;
+use Vormkracht10\LaravelOK\Checks\Base\Result;
+
+class DatabaseTableSizeCheck extends Check
+{
+    protected string $connectionName;
+
+    protected array $tableSizeThresholds = [];
+
+    public function onConnection(string $name): static
+    {
+        $this->connectionName = $name;
+
+        return $this;
+    }
+
+    public function setTableSizeThresholds(array $config): static
+    {
+        $this->tableSizeThresholds = $config;
+
+        return $this;
+    }
+
+    public function run(): Result
+    {
+        $result = Result::new();
+
+        $connectionName = $this->connectionName ?? config('database.default');
+
+        $connection = app(ConnectionResolverInterface::class)->connection($connectionName);
+
+        $config = array_map(
+            fn ($MB) => $MB * 1024 * 1024,
+            $this->tableSizeThresholds,
+        );
+
+        foreach ($config as $table => $max) {
+            $size = $this->getTableSize($connection, $table);
+
+            if ($size > $max) {
+                $mb = fn ($bytes) => round($bytes / 1024 / 1024, 2);
+
+                return $result->failed("Table [{$table}] size is {$mb($size)} megabytes, max is configured at {$mb($max)} megabytes");
+            }
+        }
+
+        return $result->ok();
+    }
+
+    protected function getTableSize(ConnectionInterface $connection, string $table): int
+    {
+        return match (true) {
+            $connection instanceof MySqlConnection => $connection->selectOne('SELECT (data_length + index_length) AS size FROM information_schema.TABLES WHERE table_schema = ? AND table_name = ?', [
+                $connection->getDatabaseName(),
+                $table,
+            ])->size,
+            $connection instanceof PostgresConnection => $connection->selectOne('SELECT pg_total_relation_size(?) AS size;', [
+                $table,
+            ])->size,
+            default => throw new \Exception("This database type is not supported"),
+        };
+    }
+}
